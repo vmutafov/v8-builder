4fb1bb958b12dd8017b050169b265a252f72c95c
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-08-28 12:55:51 +0300

    use gm

--- builders/v8-android-arm-v8.sh
+++ builders/v8-android-arm-v8.sh
@@ -33,18 +33,31 @@ fetch v8
 echo "target_os = ['android']" >> .gclient
 cd ~/v8/v8
 git checkout $VERSION
-./build/install-build-deps-android.sh
+# ./build/install-build-deps-android.sh
 gclient sync
 
 
-echo "=====[ Building V8 ]====="
-python ./tools/dev/v8gen.py arm64.release -vv -- '
-target_os = "android"
-target_cpu = "arm64"
-v8_target_cpu = "arm64"
-is_component_build = false
-'
-ninja -C out.gn/arm64.release -t clean
-ninja -C out.gn/arm64.release v8_libplatform
-ninja -C out.gn/arm64.release v8
+
+#######
+
+alias gm=/path/to/v8/tools/dev/gm.py
+gm android_arm64.release
+
+#######
+
+
+# echo "=====[ Building V8 ]====="
+# python ./tools/dev/v8gen.py arm64.release -vv -- '
+# target_os = "android"
+# target_cpu = "arm64"
+# v8_target_cpu = "arm64"
+# is_component_build = false
+# '
+
+# ninja -C out.gn/arm64.release d8
+
+
+# ninja -C out.gn/arm64.release -t clean
+# ninja -C out.gn/arm64.release v8_libplatform
+# ninja -C out.gn/arm64.release v8
 # cp ./third_party/android_ndk/sources/cxx-stl/llvm-libc++/libs/arm64-v8a/libc++_shared.so ./out.gn/arm64.release
